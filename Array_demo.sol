fef9336b335de2a575507f94a8f22a1260f51765
Sachin22 <sachinx1322@gmail.com>
2026-08-08 22:52:14 +0530

    Add get_names function to return names array

--- Array_demo.sol
+++ Array_demo.sol
@@ -17,5 +17,8 @@ contract Array_Demo{
     function get_numbers () public view returns (uint [] memory) {
         return numbers;
     }
+    function get_names () public view returns (string [] memory) {
+        return names;
+    }
 
 }
