bfebba2dad650391689c3eb8134cdc495efd3a8e
Sachin22 <sachinx1322@gmail.com>
2026-08-27 22:55:10 +0530

    Remove get_balance function from Bank_system
    Removed get_balance function to restrict access to user balance information.

--- Bank_system.sol
+++ Bank_system.sol
@@ -33,7 +33,5 @@ contract Bank{
         Get[msg.sender].name = name;
         Get[msg.sender].balance -= amount;
     }
-    function get_balance(address user) public view onlyowner returns (string memory, uint) {
-        return (Get[user].name, Get[user].balance);
-    }
+    
 }
